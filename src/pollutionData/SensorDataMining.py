2b323f4a3cc919b3680e8d8d25f70f31aa409a16
Niklas Theis <39201560+TheisNiklas@users.noreply.github.com>
2023-04-28 13:47:43 +0200

    add first part of pollution data

--- src/pollutionData/SensorDataMining.py
+++ src/pollutionData/SensorDataMining.py
@@ -1,83 +1,70 @@
 # Methode um alle Messdaten eines Sensors abzurufen und in einer CSV-Datei zu speichern.
-import requests
-import json
 import csv
+import os
 import time
+from typing import Any, Dict, List
+
 import pandas as pd
+import requests
 
-def get_all_sensor_data(sensor_id, date_from, date_to, sleep):
-  list_of_sensor_data = []
-  api = 'https://api.gios.gov.pl/pjp-api/v1/rest/archivalData/getDataBySensor/{sensorID}?size=500&dateFrom={dateFrom}&dateTo={dateTo}'
-  full_api = api.format(sensorID=sensor_id, dateFrom=date_from, dateTo=date_to)
-  response = requests.get(full_api)
-  while not response.ok:
-    time.sleep(60)
-    print("Ran in timeout")
-    response = requests.get(full_api)
-  
-  resp = response.json()
-  for item in resp["Lista archiwalnych wyników pomiarów"]:
-    list_of_sensor_data.append({"stationName":item["Nazwa stacji"],"stationCode": item["Kod stanowiska"], "date": item["Data"], "value": item["Wartość"]})
-  
-  fileName = ("Sensordata_id_%d.csv" % (sensor_id))
-  
-  if list_of_sensor_data:
-    keys = list_of_sensor_data[0].keys()
-    with open(fileName, 'w') as output_file:
-      dict_writer = csv.DictWriter(output_file, keys)
-      dict_writer.writeheader()
-      dict_writer.writerows(list_of_sensor_data)
-  
-    if resp["links"]["self"] == resp["links"]["last"]:
-      return
+
+def get_all_sensor_data(sensor: pd.Series, date_from: str, date_to: str):
+    api = "https://api.gios.gov.pl/pjp-api/v1/rest/archivalData/getDataBySensor/{sensorId}?size=500&dateFrom={dateFrom}&dateTo={dateTo}"
+
+    station_id = sensor["stationID"]
+    sensor_id = sensor["sensorID"]
+
+    print(f"Station {station_id}: Starting sensor {sensor_id}")
+    url = api.format(sensorId=sensor_id, dateFrom=date_from, dateTo=date_to)
+
+    fileName = f"data/pollution/2022_{station_id}_{sensor_id}.csv"
+
+    get_all_sensor_data_with_link(url, fileName)
+
+
+def get_all_sensor_data_with_link(link_next: str, file_name: str):
+    list_of_sensor_data: List[Dict[str, Any]] = []
+    try:
+        response = requests.get(link_next, timeout=60)
+    except Exception as e:
+        pass
+
+    while not response.ok:
+        if response.status_code == 500:
+            return
+        time.sleep(60)
+        print("Ran into timeout")
+        response = requests.get(link_next, timeout=60)
+
+    response_json = response.json()
+
+    for item in response_json["Lista archiwalnych wyników pomiarów"]:
+        list_of_sensor_data.append(
+            {
+                "date": item["Data"],
+                "value": item["Wartość"],
+            }
+        )
+
+    if len(list_of_sensor_data) > 0:
+        keys = list_of_sensor_data[0].keys()
+        with open(file_name, "a", encoding="utf-8", newline="") as output_file:
+            dict_writer = csv.DictWriter(output_file, keys)
+            if os.stat(file_name).st_size == 0:
+                dict_writer.writeheader()
+            dict_writer.writerows(list_of_sensor_data)
+
+    if response_json["links"]["self"] == response_json["links"]["last"] or response_json["totalPages"] == 0:
+        return
     else:
-      #To avoid Timeout
-      time.sleep(sleep)
-      get_all_sensor_data_with_link(resp["links"]["next"], fileName, keys, sleep)
-  else:
-    open(("Empty_" + fileName), 'a').close()
-    return
+        time.sleep(31)
+        get_all_sensor_data_with_link(response_json["links"]["next"], file_name)
 
 
+def crawling(station_df: pd.DataFrame, date_from, date_to):
+    for _, sensor in station_df.iterrows():
+        get_all_sensor_data(sensor, date_from, date_to)
 
 
-
-def get_all_sensor_data_with_link(link_next, file_Name, keys, sleep):
-  list_of_sensor_data = []
-  response = requests.get(link_next)
-
-  while not response.ok:
-    time.sleep(60)
-    print("Ran in timeout")
-    response = requests.get(link_next)
-
-  resp = response.json()
-
-  for item in resp["Lista archiwalnych wyników pomiarów"]:
-    list_of_sensor_data.append({"stationName":item["Nazwa stacji"],"stationCode": item["Kod stanowiska"], "date": item["Data"], "value": item["Wartość"]})
-
-  with open(file_Name, 'a') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writerows(list_of_sensor_data)
-
-  if resp["links"]["self"] == resp["links"]["last"]:
-    return
-  else:
-    #To avoid Timeout
-    time.sleep(sleep)
-    get_all_sensor_data_with_link(resp["links"]["next"], file_Name, keys, sleep)
-
-
-def crawling(id_list, date_from, date_to, sleep):
-  to = len(id_list)
-  crawling2(id_list, date_from, date_to, 0, to, 31)
-
-def crawling2(id_list, date_from, date_to, from_id, to_id, sleep):
-  for id in id_list[from_id: to_id]:
-    get_all_sensor_data(id, date_from, date_to, 31)
-    time.sleep(sleep)
-
-
-# df = pd.read_csv("allSensors.csv")
-# listOfAllStationIDs = df["stationID"]
-# crawling(listOfAllStationIDs, "2022-01-01 00:00", "2022-01-31 23:59", 31)
+df = pd.read_csv("allSensors.csv")
+crawling(df, "2022-01-01 00:00", "2022-12-31 23:59")
---
+++ src/pollutionData/meta_data_extraction.py
@@ -0,0 +1,72 @@
+import json
+import os
+from datetime import datetime, timedelta
+from typing import List, Tuple
+
+import pandas as pd
+
+# FIX: Path build with os.path.join
+
+
+class MetaDataExtraction:
+    data_path: str
+
+    def __init__(self, data_path: str = "../../data/pollution/"):
+        self.data_path = data_path
+
+    def extractMetaData(self):
+        files = os.listdir(self.data_path)
+        files_csv = [file for file in files if os.path.splitext(file)[1] == ".csv"]
+
+        station_data = pd.read_csv("stationID.csv")
+
+        meta_data = []
+        for file in files_csv:
+            station_id, sensor_id = self.__extractIds(file)
+            station_data_row = station_data.loc[station_data["stationID"] == station_id]
+            latitude = station_data_row["latitude"].values[0]
+            longitude = station_data_row["longitude"].values[0]
+            df = pd.read_csv(self.data_path + file)
+            valid_ranges, missing_ranges = self.__extractRanges(pd.to_datetime(df["date"], format="%Y-%m-%d %H:%M:%S"))
+            meta_data.append(
+                {
+                    "station_id": station_id,
+                    "sensor_id": sensor_id,
+                    "filename": file,
+                    "valid_ranges": valid_ranges,
+                    "missing_ranges": missing_ranges,
+                    "latitide": latitude,
+                    "longitude": longitude,
+                }
+            )
+
+        with open(self.data_path + "meta_data.json", "w", encoding="utf-8") as json_file:
+            json_file.write(json.dumps(meta_data, indent=4, default=str))
+
+    def __extractIds(self, file_name: str) -> Tuple[int, int]:
+        name, _ = os.path.splitext(file_name)
+        name_parts = name.split("_")
+        station_id = int(name_parts[1])
+        sensor_id = int(name_parts[2])
+        return (station_id, sensor_id)
+
+    def __extractRanges(
+        self, date_series: pd.Series
+    ) -> Tuple[List[Tuple[datetime, datetime, timedelta]], List[Tuple[datetime, datetime, timedelta]]]:
+        ranges: List[Tuple[datetime, datetime]] = []
+        missing_ranges: List[Tuple[datetime, datetime]] = []
+        current_start = date_series.iloc[0]
+        last_date: datetime = current_start
+        for date in date_series.iloc[1 : len(date_series)]:
+            if date - last_date > timedelta(0, 3600):
+                ranges.append((current_start, last_date, last_date - current_start))
+                current_start = date
+                missing_start = last_date + timedelta(hours=1)
+                missing_end = date - timedelta(hours=1)
+                missing_ranges.append((missing_start, missing_end, missing_end - missing_start))
+            last_date = date
+        ranges.append((current_start, last_date, last_date - current_start))
+        return (ranges, missing_ranges)
+
+
+MetaDataExtraction("data/pollution/").extractMetaData()
